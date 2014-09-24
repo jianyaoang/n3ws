1f90515e73773f6814ab60074d0db470b7e07aa4
jianyaoang <jianyaoang1@gmail.com>
2014-09-24 16:32:40 +0800

    minor code cleaning

--- n3ws/MenuViewController.m
+++ n3ws/MenuViewController.m
@@ -35,11 +35,6 @@
     self.tableView.separatorColor = [UIColor clearColor];
 }
 
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.menuTitles.count;
@@ -52,47 +47,7 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
     cell.textLabel.text = [self.menuTitles objectAtIndex:indexPath.row];
     return cell;
-    
-//    static NSString *cellID = @"MenuCell";
-//    
-//    switch (indexPath.row)
-//    {
-//        case 0:
-//            cellID = @"Headlines";
-//            break;
-//        case 1:
-//            cellID = @"Finance";
-//            break;
-//        case 2:
-//            cellID = @"Entertainment";
-//            break;
-//        case 3:
-//            cellID = @"Sports";
-//            break;
-//        default:
-//            break;
-//    }
-//    
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MenuCell" forIndexPath:indexPath];
-//    
-//    if ([cellID isEqualToString:@"Headlines"])
-//    {
-//        cell.textLabel.text = @"Headlines";
-//    }
-//    else if ([cellID isEqualToString:@"Finance"])
-//    {
-//        cell.textLabel.text = @"Finance";
-//    }
-//    else if ([cellID isEqualToString:@"Entertainment"])
-//    {
-//        cell.textLabel.text = @"Entertainment";
-//    }
-//    else if ([cellID isEqualToString:@"Sports"])
-//    {
-//        cell.textLabel.text = @"Sports";
-//    }
-//    
-//    return cell;
+
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -100,30 +55,6 @@
     NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
     UINavigationController *destViewController = (UINavigationController*)segue.destinationViewController;
     destViewController.title = [self.menuTitles objectAtIndex:indexPath.row];
-    
-    
-//    UINavigationController *navigationController = segue.destinationViewController;
-//    
-//    HeadlinesViewController *hvc = [navigationController childViewControllers].firstObject;
-//    
-//    if ([hvc isKindOfClass:[HeadlinesViewController class]])
-//    {
-//        hvc.title = @"Headlines";
-//    }
-   
-    
-//        if ([viewController isKindOfClass:[HeadlinesViewController class]])
-//        {
-//            HeadlinesViewController *hvc = (HeadlinesViewController*)viewController;
-//            hvc.title = @"Headlines";
-//            [self.navigationController popToViewController:hvc animated:YES];
-//        }
-//        else if ([viewController isKindOfClass:[MainViewController class]])
-//        {
-//            MainViewController *mvc = (MainViewController*)viewController;
-//            mvc.title = @"N3ws";
-//            [self.navigationController popToViewController:mvc animated:YES];
-//        }
 }
 
 @end
