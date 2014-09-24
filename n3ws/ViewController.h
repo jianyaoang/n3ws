7dc2ea67c93464dbc31ac4b6c0b3c482a2f7882b
jianyaoang <jianyaoang1@gmail.com>
2014-09-24 13:49:51 +0800

    add files of n3ws project

---
+++ n3ws/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  n3ws
+//
+//  Created by Jian Yao Ang on 9/24/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ n3wsTests/n3wsTests.m
@@ -0,0 +1,34 @@
+//
+//  n3wsTests.m
+//  n3wsTests
+//
+//  Created by Jian Yao Ang on 9/24/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface n3wsTests : XCTestCase
+
+@end
+
+@implementation n3wsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
